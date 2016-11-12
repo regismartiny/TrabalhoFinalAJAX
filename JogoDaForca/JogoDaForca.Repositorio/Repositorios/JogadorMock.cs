dea040f82d0078c4c80f0531cb82af7b849758f3
Unknown <regismartiny@gmail.com>
2016-11-12 09:31:01 -0200

    feat(JogoDaForca): adicionar repositorios mock.

---
+++ JogoDaForca/JogoDaForca.Repositorio/Repositorios/JogadorMock.cs
@@ -0,0 +1,39 @@
+﻿using JogoDaForca.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JogoDaForca.Dominio.ClassesDb;
+
+namespace RepositorioMock.Mock
+{
+    class JogadorMock : IJogadorRepositorio
+    {
+        private IList<Jogador> jogador;
+
+        public JogadorMock()
+        {
+            jogador = new List<Jogador>();
+            Jogador jogador1 = new Jogador() { Id = 1, Nome = "Rafael", Senha = "123" };
+            Jogador jogador2 = new Jogador() { Id = 2, Nome = "Rodrigo", Senha = "1234" };
+            Jogador jogador3 = new Jogador() { Id = 3, Nome = "Regis", Senha = "12345" };
+            Jogador jogador4 = new Jogador() { Id = 4, Nome = "Otavio", Senha = "123456" };
+
+            jogador.Add(jogador1);
+            jogador.Add(jogador2);
+            jogador.Add(jogador3);
+            jogador.Add(jogador4);
+        }
+
+        public Jogador BuscarPorNome(string nome)
+        {
+            return this.jogador.FirstOrDefault(jogador => jogador.Nome.Equals(nome));
+        }
+
+        public Jogador BuscarPorID(int id)
+        {
+            return this.jogador.FirstOrDefault(jogador => jogador.Id == id);
+        }
+    }
+}
---
+++ JogoDaForca/JogoDaForca.Repositorio/Repositorios/PalavraMock.cs
@@ -0,0 +1,35 @@
+﻿using JogoDaForca.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JogoDaForca.Dominio.ClassesDb;
+
+namespace RepositorioMock
+{
+    class PalavraMock : IPalavraRepositorio
+    {
+        private IList<Palavra> listaDePalavras;
+
+        public PalavraMock()
+        {
+            listaDePalavras = new List<Palavra>();
+            Palavra p1 = new Palavra() { Id = 1, Nome = "Pneu", Dificuldade = Dificuldade.Normal};
+            Palavra p2 = new Palavra() { Id = 2, Nome = "Bicicleta", Dificuldade = Dificuldade.Normal };
+            Palavra p3 = new Palavra() { Id = 3, Nome = "Rimel", Dificuldade = Dificuldade.BH };
+            Palavra p4 = new Palavra() { Id = 4, Nome = "Hieroglifo", Dificuldade = Dificuldade.BH };
+
+            listaDePalavras.Add(p1);
+            listaDePalavras.Add(p2);
+            listaDePalavras.Add(p3);
+            listaDePalavras.Add(p4);
+        }
+
+        public Palavra BuscarPalavra(IList<Palavra> palavrasJaUsadas, Dificuldade dificuldade)
+        {
+            var listaFiltrada = listaDePalavras.Where(p => palavrasJaUsadas.Any(filtro => filtro.Id != p.Id));
+            Palavra palavra =  listaFiltrada.Where(p => p.Dificuldade == dificuldade).OrderBy(c => Guid.NewGuid()).FirstOrDefault();
+            palavrasJaUsadas.Add(palavra);
+            return palavra;
+        }
+    }
+}
