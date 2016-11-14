e01f5459eb25e5f3edfc95970937c9d00c9ea3e6
Otávio <otaviobubans@hotmail.com>
2016-11-13 23:15:50 -0200

    Feat(BDForca): sql com palavras

---
+++ BDForca/ForcaBD.sql
@@ -0,0 +1,90 @@
+CREATE TABLE Palavras(
+	IdPalavra	int Identity NOT NULL,
+	Nome		varchar(100) NOT NULL,
+	Dificuldade varchar(50) NOT NULL
+);
+
+insert into Palavras Values('paris', 'Normal');
+insert into Palavras Values('dado', 'Normal');
+insert into Palavras Values('bola', 'Normal');
+insert into Palavras Values('banana', 'Normal');
+insert into Palavras Values('abacaxi', 'Normal');
+insert into Palavras Values('chocolate', 'Normal');
+insert into Palavras Values('guloseima', 'Normal');
+insert into Palavras Values('janela', 'Normal');
+insert into Palavras Values('porta', 'Normal');
+insert into Palavras Values('retrovisor', 'Normal');
+insert into Palavras Values('almanaque', 'Normal');
+insert into Palavras Values('eclipse', 'Normal');
+insert into Palavras Values('netbeans', 'Normal');
+insert into Palavras Values('bluej', 'Normal');
+insert into Palavras Values('ajax', 'Normal');
+insert into Palavras Values('detergente', 'Normal');
+insert into Palavras Values('almofada', 'Normal');
+insert into Palavras Values('estante', 'Normal');
+insert into Palavras Values('computador', 'Normal');
+insert into Palavras Values('caneca', 'Normal');
+insert into Palavras Values('jarra', 'Normal');
+insert into Palavras Values('talher', 'Normal');
+insert into Palavras Values('mouse', 'Normal');
+insert into Palavras Values('cobertor', 'Normal');
+insert into Palavras Values('camiseta', 'Normal');
+insert into Palavras Values('macaco', 'Normal');
+insert into Palavras Values('cachorro', 'Normal');
+insert into Palavras Values('gato', 'Normal');
+insert into Palavras Values('elefante', 'Normal');
+insert into Palavras Values('girafa', 'Normal');
+insert into Palavras Values('notebook', 'Normal');
+insert into Palavras Values('varal', 'Normal');
+insert into Palavras Values('prendedor', 'Normal');
+insert into Palavras Values('roupa', 'Normal');
+insert into Palavras Values('camiseta', 'Normal');
+insert into Palavras Values('tenis', 'Normal');
+insert into Palavras Values('bermuda', 'Normal');
+insert into Palavras Values('trompete', 'Normal');
+insert into Palavras Values('pandeiro', 'Normal');
+insert into Palavras Values('netflix', 'Normal');
+insert into Palavras Values('amazon', 'Normal');
+insert into Palavras Values('facebook', 'Normal');
+insert into Palavras Values('twitter', 'Normal');
+insert into Palavras Values('slack', 'Normal');
+insert into Palavras Values('estrela', 'Normal');
+insert into Palavras Values('terra', 'Normal');
+insert into Palavras Values('marte', 'Normal');
+insert into Palavras Values('netuno', 'Normal');
+insert into Palavras Values('alfabeto', 'Normal');
+insert into Palavras Values('ingrime', 'Normal');
+insert into Palavras Values('ornitorrinco', 'BH');
+insert into Palavras Values('paralelepipido', 'BH');
+insert into Palavras Values('caminhoneiro', 'BH');
+insert into Palavras Values('catarinense', 'BH');
+insert into Palavras Values('canavicultor', 'BH');
+insert into Palavras Values('cancerologia', 'BH');
+insert into Palavras Values('cancerofobia', 'BH');
+insert into Palavras Values('assincronismo', 'BH');
+insert into Palavras Values('catastrofico', 'BH');
+insert into Palavras Values('pacificidade', 'BH');
+insert into Palavras Values('paleontologia', 'BH');
+insert into Palavras Values('latifundiario', 'BH');
+insert into Palavras Values('otorrinolaringologista', 'BH');
+insert into Palavras Values('desentupidor', 'BH');
+insert into Palavras Values('desentupidor', 'BH');
+insert into Palavras Values('escorregador', 'BH');
+insert into Palavras Values('estetoscópio', 'BH');
+insert into Palavras Values('desfibrilador', 'BH');
+insert into Palavras Values('estabilizador', 'BH');
+insert into Palavras Values('achocolatado   ', 'BH');
+insert into Palavras Values('profiteroles   ', 'BH');
+insert into Palavras Values('refrigerante   ', 'BH');
+insert into Palavras Values('procastinas', 'BH');
+insert into Palavras Values('conhecimento', 'BH');
+insert into Palavras Values('parcialmente', 'BH');
+insert into Palavras Values('inexplicavel', 'BH');
+insert into Palavras Values('motocondrias', 'BH');
+insert into Palavras Values('pluviosidade', 'BH');
+insert into Palavras Values('implementacao', 'BH');
+insert into Palavras Values('plausibilidade', 'BH');
+
+
+	
+
